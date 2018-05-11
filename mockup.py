5bb943009f79bd67ac6b5f7b9cd0fcc140397371
William Harrell <wharrell1@me.com>
2018-05-11 11:51:10 -0400

    stopped adam's whining

--- mockup.py
+++ mockup.py
@@ -1,9 +1,11 @@
 class Planet(object):
     def __init__(self, i):
 
-earthInfo = [mass, radius, posx
+sun = [1.989110e30, 6.9550810e6, -1.318179998275412E+06, 4.010555758878363E+05, -4.493504117531995E-03, -1.503318013232030E-02]
+mercury = [3.302210e23, 2.439710e3, 4.183977317411777E+07, -4.422147843777118E+07, 2.534767960872066E+01, 3.616526476323813E+01]
+venus = [4.868510e24, 6.051810e3, -8.208050566798364E+07, -7.152072408769740E+07, 2.303847371249511E+01, -2.633317430531520E+01]
 
-planets = [sun, venus, mercury, earth, mars, jupiter, saturn, neptune, uranus, pluto]
+planets = [sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto]
 
 dt = 3600 # 1 Hour
 
@@ -23,4 +25,3 @@ class Planet(object):
         self.p += self.force*dt
         self.vel = self.p/self.mass
         self.pos += self.vel*dt
-
