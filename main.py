c18cc7c6fe1a2696311ce7bc43f3b362a581b7c1
Team900 Jetson TX1 <progammers@team900.org>
2018-05-17 08:20:09 -0400

    Updated solar system model including Z-values

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from vpython import *
 
-AE = 1000*149597871
-G = 6.67384e-11
+AU = 149597871000
+G = 6.67384E-11
+
+scene.width = 1600
+scene.height = 900
+
+def keyInput(evt):
+    s = evt.key
+    if s == 'esc':
+        scene.waitfor('keydown')
+
+scene.bind('keydown', keyInput)
 
 def forceOf(planet, otherPlanet):
     global G
@@ -16,30 +26,37 @@ def updatePlanet(planet, dt):
     planet.pos += planet.velocity*dt
 
 data = open("data.txt", 'r')
+planetData = []
 planetNames = []
 for line in data:
     line = str(line)[:-2]
     vals = line.split()
-    varname, mass, rad, posx, posy, velx, vely = vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6]
+    varname, mass, rad, posx, posy, posz, velx, vely, velz = vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6], vals[7], vals[8]
     planetNames.append(varname)
-    comm = varname + 'Info = [' + mass + ', ' + rad + ', ' + posx + ', ' + posy + ', ' + velx + ', ' + vely + ']'
+    comm = varname + 'Info = [' + mass + ', ' + rad + ', ' + posx + ', ' + posy + ', ' + posz + ', ' + velx + ', ' + vely + ',' + velz + ']'
+    exec(comm)
+    comm = 'planetData.append(' + varname + 'Info)'
     exec(comm)
 
 print(planetNames)
 
+for i in range(len(planetData)):
+    print(list(locals().keys())[list(locals().values()).index(planetData[i])], '=', planetData[i])
+
 planets = []
+planetLabels = []
 
 for i in range(len(planetNames)):
-    comm = planetNames[i] + 'pos = vec(' + planetNames[i] + 'Info[2] , ' + planetNames[i] + 'Info[3], 0)'
+    comm = planetNames[i] + 'pos = vec(' + planetNames[i] + 'Info[2] , ' + planetNames[i] + 'Info[3], ' + planetNames[i] + 'Info[4])'
     exec(comm)
-    comm = planetNames[i] + 'vel = vec(' + planetNames[i] + 'Info[4] , ' + planetNames[i] + 'Info[5], 0)'
+    comm = planetNames[i] + 'vel = vec(' + planetNames[i] + 'Info[5] , ' + planetNames[i] + 'Info[6], ' + planetNames[i] + 'Info[7])'
     exec(comm)
     comm = 'R' + planetNames[i] + ' = ' + planetNames[i] + 'Info[1]'
     exec(comm)
     comm = 'M' + planetNames[i] + ' = ' + planetNames[i] + 'Info[0]'
     exec(comm)
 
-    comm = planetNames[i] + ' = sphere(pos=' + planetNames[i] + 'pos, radius=R' + planetNames[i] + ', color=color.green, make_trail=True)'
+    comm = planetNames[i] + ' = sphere(pos=' + planetNames[i] + 'pos, radius=R' + planetNames[i] + ', color=color.green, make_trail=True, retain=1000)'
     exec(comm)
     
     comm = planetNames[i] + '.mass = M' + planetNames[i]
@@ -52,27 +69,34 @@ for i in range(len(planetNames)):
     exec(comm)
 
 tstr = "Time: {:.0f} days".format(0)
-tlabel = label(pos=vector(0, 1.2*mag(marspos), 0), text=tstr)
+tlabel = label(pos=vector(0, 20*AU, 0), text=tstr)
 launchstr = 'Click to Start'
-launchlabel=label(pos=vector(0, -1.2*mag(marspos), 0), text=launchstr)
+launchlabel=label(pos=vector(0, -20*AU, 0), text=launchstr)
 
 dt = 3600
 day = 24*3600
 t = 0
 
-scene.autoscale = False
-
 for planet in planets:
     planet.momentum = planet.velocity*planet.mass
 
+for i in range(len(planetNames)):
+    comm = 'pstr = "' + planetNames[i] + '"'
+    exec(comm)
+
+    comm = planetNames[i] + 'label = label(pos=vector(0, 1.2*R' + planetNames[i] + ', 0) + ' + planetNames[i] + 'pos , text = pstr)'
+    exec(comm)
+
+    comm = 'planetLabels.append(' + planetNames[i] + 'label)'
+    exec(comm)
+
 while True:
-    rate(100)
 
     if t == 0:
         launchstr = "Click to Start"
         launchlabel.text = launchstr
         scene.waitfor('click')
-        llaunchstr = "Running..."
+        launchstr = "Running..."
         launchlabel.text = launchstr
 
     for planet in planets:
@@ -81,8 +105,13 @@ while True:
             if otherPlanet is not planet:
                 planet.force += forceOf(planet, otherPlanet)
         updatePlanet(planet, dt)
-
-    print(earth.pos)
+    
+    for i, label in enumerate(planetLabels):
+        comm = 'position = vector(0, 1.2*R' + planetNames[i] + ', 0) + ' + planetNames[i] + '.pos'
+        exec(comm)
+        #print(position)
+        
+        label.pos = position
 
     t += dt
     tstr = "Time: {:0f} days".format(t/day)
